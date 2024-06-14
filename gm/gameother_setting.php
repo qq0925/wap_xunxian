8058cb339ffb7feefb79716e6ab07fca2691cb62
792774502@qq.com <792774502@qq.com>
2024-06-14 16:10:29 +0800

    Update gameother_setting.php

--- gm/gameother_setting.php
+++ gm/gameother_setting.php
@@ -10,6 +10,8 @@ $money_def = $encode->encode("cmd=gm_game_othersetting&canshu=6&sid=$sid");
 $rank_def = $encode->encode("cmd=gm_game_othersetting&canshu=7&sid=$sid");
 $battle_def = $encode->encode("cmd=gm_game_othersetting&canshu=8&sid=$sid");
 $auc_def = $encode->encode("cmd=gm_game_othersetting&auc_canshu=0&canshu=9&sid=$sid");
+$reboot_all = $encode->encode("cmd=gm_game_othersetting&canshu=10&sid=$sid");
+
 $setting_html = <<<HTML
 [功能列表]<br/><br/>
 <a href="?cmd=$area_def">分区管理</a>(✔)<br/>
@@ -21,6 +23,8 @@ $setting_html = <<<HTML
 <a href="?cmd=$player_def">玩家数据管理</a>(✔)<br/>
 <a href="?cmd=$designer_def">设计者管理</a>(✔)<br/>
 <a href="?cmd=$other_def">杂项管理</a>(✔)<br/>
+---<br/>
+<a href="?cmd=$reboot_all">清空游戏数据</a><br/>
 <br/><a href="?cmd=$gm_main">设计大厅</a><br/>
 HTML;
 echo $setting_html;
