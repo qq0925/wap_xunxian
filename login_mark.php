95097ae1c22c4438abfa2c685880acdaaf68d70e
792774502@qq.com <792774502@qq.com>
2025-03-06 12:05:31 +0800

    Update login_mark.php

--- login_mark.php
+++ login_mark.php
@@ -40,9 +40,9 @@ if ($result) {
     include './ini/xuser_ini.php';
     $a10 = ($iniFile->getItem('验证信息', 'xcmid值'));
     
-    
+if($uid ==1){
     include 'sql_update.php';
-
+}
 $sql = "SELECT username, designer FROM userinfo WHERE token = :token";
 $stmt = $dblj->prepare($sql);
 $stmt->bindParam(':token', $token);
