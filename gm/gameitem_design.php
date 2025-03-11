1103ec08ead8ad6eb7bd4a97791b44d8449ebbc4
792774502@qq.com <792774502@qq.com>
2025-03-11 14:47:00 +0800

    Update gameitem_design.php

--- gm/gameitem_design.php
+++ gm/gameitem_design.php
@@ -22,7 +22,7 @@ $gm_game_itemdefine_10 = $encode->encode("cmd=gm_game_itemdefine&gm_post_canshu=
 
 $gm_html = <<<HTML
 <a href="?cmd=$gm_game_itemdefine_9">导出物品数据到->excel</a><br/>
-<a href="?cmd=$gm_game_itemdefine_10" >导出物品数据从<-excel</a><br/>
+<a href="?cmd=$gm_game_itemdefine_10" >导入物品数据从<-excel</a><br/>
 <p>[物品设计]<br/>
 <a href="?cmd=$gm_game_itemdefine_0">定义物品类别</a><br/>
 请选择物品类别：<br/>
